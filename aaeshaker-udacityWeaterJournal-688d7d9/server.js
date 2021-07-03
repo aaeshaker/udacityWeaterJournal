0538785f3a0473884e48f09e9ee13bd3abe3235e
Abdelrahman Shaker <aaeshaker@outlook.com>
2021-07-03 21:16:43 +0200

    enhancing code

--- aaeshaker-udacityWeaterJournal-688d7d9/server.js
+++ aaeshaker-udacityWeaterJournal-688d7d9/server.js
@@ -1,6 +1,8 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+// Download in 'terminal' all packages (express, body-parser & cors)
+
 // Require Express to run server and routes
 const express = require('express');
 // Start up an instance of app
@@ -12,47 +14,54 @@ const cors = require('cors');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-app.use(cors()); 
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 // Setup Server
-const port = 8000;
+const port = 8000; // you can make it whatever you like
 const server = app.listen(port, listening);
 
 //callback function to check if the server is running
-function listening(){
+function listening() {
     console.log("server is running");
+    console.log(`running on localhost: ${port}`);
 }
 
-//GET
+//GET Route
+// get all data from server to app
+// So we call the get route here => '/all'
 app.get('/all', sendInfo);
 
-function sendInfo(req, res){
+function sendInfo(req, res) {
     res.send(projectData);
-    projectData = {};
+    projectData = {}; // Then clear the project data again
 }
 
-//POST
+//POST Route
+// post all data fom app to server
 app.post('/add', addInfo);
 
-function addInfo(request, respond){
+function addInfo(request, respond) {
+
     let data = request.body;
 
-    console.log('server side data ', data)
-    
+    console.log('server side data: ', data)
+
     //date
     //temp -> temperature
     // feelings -> user's input
-    
-    projectData["date"] = data.date;
-    projectData["temp"] = data.temp;
-    projectData["feel"] = data.feeling;
-    
+
+    projectData['date'] = data.date;
+    projectData['temp'] = data.temp;
+    projectData['feeling'] = data.feeling;
+
     respond.send(projectData);
 }
--- aaeshaker-udacityWeaterJournal-688d7d9/website/app.js
+++ aaeshaker-udacityWeaterJournal-688d7d9/website/app.js
@@ -1,67 +1,98 @@
 // Create a new date instance dynamically with JS
 let day = new Date();
-let newDate = (day.getMonth() + 1) + '/'+ day.getDate()+'/'+ day.getFullYear();
+let newDate = (day.getMonth() + 1) + '/' + day.getDate() + '/' + day.getFullYear();
 
 /* Global Variables */
 const apiUrl = "http://api.openweathermap.org/data/2.5/forecast?zip=";
-const apiKey = "&appid=7130c3c7cc45085627bea6bbdeac4bd9&units=imperial";
-
-//function to GET API Data
-const getWeather = async (apiUrl, zipCode, key) => {
-    const resource = await fetch(apiUrl+zipCode+key);
-    try{
-        const data = await resource.json();
-        return data;
-    }catch(error){
-        console.log("error", error);
-    }
-}
-
-// function to POST Data
-const postData = async ( url = '', data = {}) =>{
-    console.log(data);
-    const response = await fetch(url, {
-        method: 'POST',
-        credentials : 'same-origin',
-        headers: {
-            'Content-Type' : 'application/json',
-        },
-        body: JSON.stringify(data)
-    });
-
-    try{
-        const newData = await response.json();
-        console.log(newData);
-        return newData;
-    }catch(error){
-        console.log("the request failed");
-    }
-}
-
-//function to display data
-const updateUI = async()=>{
-    const request = await fetch('/all')
-    try{
-    const allData = await request.json();
-    console.log(allData)
-    document.getElementById('date').innerHTML = `Date:${allData.date}`;
-    document.getElementById('temp').innerHTML = `Temperature:${allData.temp}`;
-    document.getElementById('content').innerHTML = `I feel:${allData.feel}`;
-    }catch(err){
-    console.log('error',err);
-    }
-    }
+const apiKey = "&appid=83af79aadf896b70eee432bbcb104b0d";
 
 //add EventListener
-document.getElementById("generate").addEventListener('click', action)
+document.getElementById("generate").addEventListener('click', action);
 
-function action(){
+function action() {
+
     const feelings = document.getElementById("feelings").value;
     const newZipCode = document.getElementById("zip").value;
 
     getWeather(apiUrl, newZipCode, apiKey)
-    .then((data) => {
-        postData('/add', {date: newDate, temp: data.list[0].main.temp, feeling: feelings})
-        updateUI();
-    })
+
+        // Step 1: get data (temprature) from API
+        .then((data) => {
+
+            // Step 2: post data (date, temprature & feelings ) to server
+            postData('/add', {
+                date: newDate,
+                temp: data.list[0].main.temp,
+                feeling: feelings
+            });
+
+            // Step 3: get data (date, temprature & feelings ) from server to update UI
+            updateUI();
+        })
+
 };
+
+//function to GET API Data
+const getWeather = async (apiUrl, zipCode, key) => {
+
+    const resource = await fetch(apiUrl + zipCode + key);
+
+    try {
+
+        const data = await resource.json(); // convert data to JSON
+        return data;
+
+    } catch (error) {
+
+        console.log("error", error);
+
+    }
+
+}
+
+// function to POST Data
+const postData = async (url = '', data = {}) => {
+
+    console.log(data);
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    try {
+
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+
+    } catch (error) {
+
+        console.log("the request failed");
+
+    }
+}
+
+//function to display data in UI
+const updateUI = async () => {
+
+    const request = await fetch('/all');
+
+    try {
+
+        const allData = await request.json();
+        console.log(allData);
+        document.getElementById('date').innerHTML = `Date:${allData.date}`;
+        document.getElementById('temp').innerHTML = `Temperature:${allData.temp}`;
+        document.getElementById('content').innerHTML = `I feel:${allData.feeling}`;
+
+    } catch (err) {
+
+        console.log('error', err);
+
+    }
+}
